7b3682e2ddbe1326bc6eeab2d73f49ea25e298d7
antoni <antoni.dev0@gmail.com>
2024-10-15 16:49:18 -0700

    create file name_generator.py for starter code

---
+++ band_name_generator.py
@@ -0,0 +1,3 @@
+response_city = input("what is the name of the city you group up in?\n")
+response_pet = input("what is the name of your pet?\n")
+print("Your band name is " + response_city + " " + response_pet)
